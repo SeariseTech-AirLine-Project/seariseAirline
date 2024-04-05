d18f619530ffbf64b7d4e0c969998e1a124689e2
SeariseTech <searisetech@gmail.com>
2024-04-04 20:46:46 -0400

    new file added

---
+++ mylog.sh
@@ -0,0 +1,12 @@
+vi fileMgT2.sh
+#!/bin/bash
+#fileMGT Script for Searise web-server
+sudo mkdir /var/www/html/enrolment
+sudo mkdir /var/www/html/class
+sudo mkdir /var/www/html/trainning
+sudo mkdir /var/www/html/graduation
+sudo echo "<h1> Welcom to Searise Technology Solution Enrolment Form Page</h1>" > /var/www/html/enrolment/index.html
+sudo echo "Welcom to Searise Technology Solution The Class Sections Start On Oct 16th 2023" > /var/www/html/class/index.html
+sudo echo "Welcom to Searise Technology Solution All Students Shall Under Go Hand On Trainning With Prof Adeyinka" > /var/www/html/trainning/index.html
+sudo echo "Welcom to Searise Technology Solution Every Student Shall Graduate with Many Job Offers By God's Grace" > /var/www/html/graduation/index.html
+
